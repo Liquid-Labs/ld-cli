d9e56d2b91ec2ef659037569788a1cf326ab4848
Zane Rockenbaugh <zane@liquid-labs.com>
2022-10-10 22:38:06 -0500

    hack in output handler

--- src/liq2/liq2.mjs
+++ src/liq2/liq2.mjs
@@ -2,5 +2,17 @@ import { processCommand } from './lib'
 
 const args = process.argv.slice(2)
 
+const [ method, path, data, url ] = processCommand(args);
 
-const [ method, path, data, url ] = processCommand(args)
+console.log(url);
+
+(async () => {
+  const response = await fetch(url)
+  const contentType = response.headers.get('content-type')
+  if (contentType.startsWith('application/json')) {
+    // console.log(JSON.stringify(await response.json(), null, '  '))
+  }
+  else {
+    // console.log(await response.text())
+  }
+})()
