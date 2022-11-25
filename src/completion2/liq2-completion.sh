e5596014d6b2b93782166492504a2e5d0f7c686c
Zane Rockenbaugh <zane@liquid-labs.com>
2022-11-25 08:29:48 -0600

    work out the completed bit of the command; first draft liq2 completion

--- src/completion2/liq2-completion.sh
+++ src/completion2/liq2-completion.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/env bash
 
 _liq2() {
-  local COMPLETE_BIT="${COMP_WORDS[COMP_CWORD-1]}"
+  local COMPLETED_COMMAND
+  local i=0
+  while (( ${i} < ${COMP_CWORD} )); do
+    if (( ${i} == 0 )); then
+      COMPLETED_COMMAND="${COMP_WORDS[0]}"
+    else
+      COMPLETED_COMMAND="${COMPLETED_COMMAND} ${COMP_WORDS[${i}]}"
+    fi
+    i=$(( ${i} + 1 ))
+  done
+  
   local CURR_COMMAND="${COMP_WORDS[COMP_CWORD]}"
   local OPTS
-  OPTS=$(liq2 server next-commands -- command="${COMPLETE_BIT}")
+  OPTS=$(liq2 server next-commands -- command="${COMPLETED_COMMAND}")
   
   COMPREPLY=( $(compgen -W "${OPTS}" -- ${CURR_COMMAND}) )
 }
