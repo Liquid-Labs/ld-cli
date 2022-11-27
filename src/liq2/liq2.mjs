f4e356fff13ee576ba011fe504aef17017da2334
Zane Rockenbaugh <zane@liquid-labs.com>
2022-11-26 20:15:37 -0600

    now smartly saves attachment responses

--- src/liq2/liq2.mjs
+++ src/liq2/liq2.mjs
@@ -1,3 +1,6 @@
+import * as fs from 'node:fs/promises'
+import * as sysPath from 'node:path'
+
 import { processCommand } from './lib'
 
 const args = process.argv.slice(2)
@@ -10,11 +13,24 @@ const { accept, method, path, data, url } = processCommand(args);
       'Accept': accept
     }
   })
-  const contentType = response.headers.get('content-type')
-  if (contentType.startsWith('application/json')) {
-    console.log(JSON.stringify(await response.json(), null, '  '))
+  const contentType = response.headers.get('Content-Type')
+  const disposition = response.headers.get('Content-Disposition')
+  if (disposition?.startsWith('attachment')) { // save the file
+    let outputFileName = 'output'
+    const [ , fileNameBit ] = disposition.split(/;\s*/)
+    if (fileNameBit.startsWith('filename=')) {
+      const [ , rawFileName ] = fileNameBit.split(/=\s*/)
+      outputFileName = sysPath.basename(rawFileName)
+    }
+    
+    await fs.writeFile(outputFileName, (await response.blob()).stream())
   }
-  else {
-    console.log(await response.text())
+  else { // output to screen
+    if (contentType.startsWith('application/json')) {
+      console.log(JSON.stringify(await response.json(), null, '  '))
+    }
+    else {
+      console.log(await response.text())
+    }
   }
 })()
