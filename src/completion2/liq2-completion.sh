64f4814c3e32249b8fd57a7f49807c6acbcad03a
Zane Rockenbaugh <zane@liquid-labs.com>
2022-12-01 10:43:34 -0600

    greatly simplified completion algorithm

--- src/completion2/liq2-completion.sh
+++ src/completion2/liq2-completion.sh
@@ -1,22 +1,7 @@
 #!/usr/bin/env bash
 
-_liq2() {
-  local COMPLETED_COMMAND
-  local i=0
-  while (( ${i} < ${COMP_CWORD} )); do
-    if (( ${i} == 0 )); then
-      COMPLETED_COMMAND="${COMP_WORDS[0]}"
-    else
-      COMPLETED_COMMAND="${COMPLETED_COMMAND} ${COMP_WORDS[${i}]}"
-    fi
-    i=$(( ${i} + 1 ))
-  done
-  
-  local CURR_COMMAND="${COMP_WORDS[COMP_CWORD]}"
-  local OPTS
-  OPTS=$(liq2 server next-commands -- command="${COMPLETED_COMMAND}")
-  
-  COMPREPLY=( $(compgen -W "${OPTS}" -- ${CURR_COMMAND}) )
+_liq2() {  
+  COMPREPLY=( $(liq2 server next-commands -- command="${COMP_LINE}") )
 }
 
 complete -F _liq2 liq2
