2db017ad570c2ea20252c2f70eeca0a6444f8d39
Zane Rockenbaugh <zane@liquid-labs.com>
2023-03-21 16:09:34 -0500

    updated handling of question bundles for multiples; support question bundle 'title'

--- src/liq/liq.mjs
+++ src/liq/liq.mjs
@@ -19,8 +19,8 @@ const callEndpoint = async(args, bundle) => {
 
 const addArg = ({ args, parameter, paramType, value }) => {
   if (!args.includes('--')) { args.push('--') }
-  if (paramType?.match(/bool(?:ean)?/i)) {
-    if (value === true) {
+  if (paramType?.match(/bool(?:ean)?/i) && value) {
+    if (value === true) { // We want this inside because we don't want to run outer if/else if we're bool
       args.push(parameter)
     }
   }
@@ -53,29 +53,48 @@ const addArg = ({ args, parameter, paramType, value }) => {
   const isQnA = !!response.headers.get('X-Question-and-Answer')
 
   if (isQnA) {
-    const questioner = new Questioner()
-    questioner.interogationBundle = await response.json()
+    const qnaBundles = await response.json()
+    const answerBundles = []
+    let sendBundle = false
+    for (const interrogationBundle of qnaBundles) {
+      const questioner = new Questioner({ interrogationBundle, initialParameters : interrogationBundle.env })
+      const { title, key } = interrogationBundle
 
-    await questioner.question()
-    const results = questioner.results
-    const bundle = {}
+      if (title !== undefined) {
+        process.stdout.write(formatTerminalText(`<h1>${title}<rst>\n`))
+      }
 
-    for (const { handling, parameter, paramType, value } of results) {
-      if (handling === 'parameter') {
-        addArg({ args, parameter, paramType, value })
+      await questioner.question()
+      const results = questioner.results
+
+      const bundle = {}
+      if (key !== undefined) bundle.key = key // we do this to save the characters of sending an undefined key
+
+      for (const { handling, parameter, paramType, value } of results) {
+        if (handling === 'parameter') {
+          addArg({ args, parameter, paramType, value })
+        }
+        else if (handling === 'keyedParameter') { // TODO: this isn't supported in the stack yet, but makes sense
+          addArg({ args, parameter : `${key}:${parameter}`, paramType, value })
+        }
+        else if (handling === undefined || handling === 'bundle') {
+          bundle[parameter] = value
+          sendBundle = true
+        }
       }
-      else if (handling === 'bundle') {
-        bundle[parameter] = value
-      }
+
+      answerBundles.push(bundle) // best practice is to send a key and access result values using the key, but we
+      // always push a result bundle for each question bundle to facilicate position-based processing
     }
 
-    if (Object.keys(bundle).length > 0) {
-      addArg({ args, parameter : 'answers', paramType : 'string', value : JSON.stringify(bundle) })
+    // if the answer bundle is truly empty, then we don't send it as the receiver probably doesn't support 'answers' 
+    // parameter
+    if (sendBundle === true) {
+      addArg({ args, parameter : 'answers', paramType : 'string', value : JSON.stringify(answerBundles) })
     }
 
+    process.stdout.write('\nRe-sending request with answers...\n')
     response = await (callEndpoint(args))
-
-    process.stdout.write('\nRe-sending request with answers...')
   }
 
   const contentType = response.headers.get('Content-Type')
