305da2d5b7d6c57567f499cf4447787185eca029
Zane Rockenbaugh <zane@liquid-labs.com>
2023-05-09 09:57:38 -0500

    implement terminal wrap preferences and local liq-core detection and install

--- src/liq/lib/setup-liq-core.mjs
+++ src/liq/lib/setup-liq-core.mjs
@@ -1,12 +1,28 @@
+import { existsSync } from 'node:fs'
+import * as fsPath from 'node:path'
+
 import { install } from '@liquid-labs/npm-toolkit'
 import { formatTerminalText } from '@liquid-labs/terminal-text'
 import { wrap } from '@liquid-labs/wrap-text'
 
-import { LIQ_CORE_VERSION } from './constants'
+import { LIQ_CORE_VERSION, LIQ_PLAYGROUND } from './constants'
 
 const setupLiqCore = () => {
   console.log(formatTerminalText(wrap('Installing <code>@liquid-labs/liq-core<rst>...', { ignoreTags : true })))
-  install({ global : true, pkgs : ['@liquid-labs/liq-core'], verbose : true, version : LIQ_CORE_VERSION })
+  console.log(wrap('Checking for local installation...'))
+  let localPath
+  const testPaths = [fsPath.join(LIQ_PLAYGROUND, 'liq-core'), fsPath.join(LIQ_PLAYGROUND, 'liquid-labs', 'liq-core')]
+  for (const testPath of testPaths) {
+    if (existsSync(testPath)) {
+      localPath = testPath
+    }
+  }
+  if (localPath !== undefined) {
+    install({ global : true, pkgs : [localPath], verbose : true })   
+  }
+  else {
+    install({ global : true, pkgs : ['@liquid-labs/liq-core'], verbose : true, version : LIQ_CORE_VERSION })
+  }
 }
 
 export { setupLiqCore }
